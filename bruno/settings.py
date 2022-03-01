a8509da54f2c7d541b8730e0404e0ea6852a2c67
Bruno <bruno.orozco@ledsm.mx>
2022-03-01 10:06:46 -0600

    Changes config for render

--- bruno/settings.py
+++ bruno/settings.py
@@ -1,7 +1,7 @@
 
 import os
 from pathlib import Path
-import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -129,8 +129,6 @@ STATIC_URL = '/static/'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-#django_heroku.settings(locals(),staticfiles=False)
 
 
 STATICFILES_DIRS = (
